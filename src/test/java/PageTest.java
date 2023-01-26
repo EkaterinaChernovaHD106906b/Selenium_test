8bfd4fde6047bdee6e808449a0e0866aa9dc047f
Екатерина Чернова <hd106906bqwerty@gmail.com>
2023-01-26 18:14:55 +0300

    Selenium test was added

--- src/test/java/PageTest.java
+++ src/test/java/PageTest.java
@@ -11,30 +11,42 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class PageTest {
     private WebDriver driver;
-   @BeforeAll
-    public static void setUpAll(){
-       WebDriverManager.chromedriver().setup();
-   }
-   @BeforeEach
-    public void setUp(){
-       ChromeOptions options = new ChromeOptions();
-       options.addArguments("--disable-dev-shm-usage");
-       options.addArguments("--no-sandbox");
-      // options.addArguments("--headless");
-       driver = new ChromeDriver(options);
-       driver.get("http://u920152e.beget.tech/# ");
-   }
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-        driver = null;
+
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
     }
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        // options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        // driver.get("http://u920152e.beget.tech/# ");
+    }
+
+    /* @AfterEach
+     public void tearDown() {
+         driver.quit();
+         driver = null;
+     }*/
     @Test
-    public void loginWithEmailAndPassword(){
+    public void loginWithEmailAndPassword() {
         driver.findElement(By.cssSelector("[type='email'][name=auth_email]")).sendKeys("1679myemail@mail.ru");
         driver.findElement(By.cssSelector("[type='password'][name='auth_pass']")).sendKeys("67JhjjY");
         WebElement button = driver.findElement(By.cssSelector("[type='submit'][name='form_auth_submit']"));
         button.click();
+    }
+
+    @Test
+    public void UploadFile() {
+        driver.get("https://the-internet.herokuapp.com/upload");
+        By fileInput = By.xpath("//*[@id='file-upload'] [@name='file']");
+        String filePath = "C:\\Project\\Test\\src\\test\\java\\File_2.txt";
+        driver.findElement(fileInput).sendKeys(filePath);
+
     }
 
 }
